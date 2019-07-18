1b95fe31b3f26f61a8392af0f09d54758bd7669e
Maxim Rubis <siburny@gmail.com>
2019-07-18 18:11:07 -0400

    Api Metadata: add new fields

--- wp-content/plugins/api-metadata/api-metadata.php
+++ wp-content/plugins/api-metadata/api-metadata.php
@@ -4,76 +4,100 @@
  * Description: Plugin that performs an API request to a shopping platform and returns requested metadata fields.
  * Author: Maxim Rubis
  * Author URI: https://rub.is/
- * Version: 0.0.1
+ * Version: 0.2.0
  */
 
 function api_metadata_shortcode($atts, $content = '')
 {
-	if(isset($atts['amazon']) && isset($atts['show']))
-	{
-		$data = make_amazon_request($atts['amazon']);
-		if(!empty($data))
-		{
-			switch($atts['show'])
-			{
-				case 'date':
-					if(!isset($atts['display']))
-					{
-						return $data['Items']['Item']['ItemAttributes']['ReleaseDate'];
-					}
-					else
-					{
-						return format_date($data['Items']['Item']['ItemAttributes']['ReleaseDate'], $atts['display']);
-					}
-				case 'director':
-					return $data['Items']['Item']['ItemAttributes']['Director'];
-				case 'title':
-					return $data['Items']['Item']['ItemAttributes']['Title'];
-			}
-		}
-	}
+    if (isset($atts['amazon']) && isset($atts['show'])) {
+        $data = make_amazon_request($atts['amazon']);
+        if (!empty($data)) {
+            switch ($atts['show']) {
+                case 'date':
+                    if (!isset($atts['display'])) {
+                        return $data['Items']['Item']['ItemAttributes']['ReleaseDate'];
+                    } else {
+                        return format_date($data['Items']['Item']['ItemAttributes']['ReleaseDate'], $atts['display']);
+                    }
+
+                case 'director':
+                    return $data['Items']['Item']['ItemAttributes']['Director'];
+
+                case 'title':
+                    return $data['Items']['Item']['ItemAttributes']['Title'];
+
+                case 'url':
+                    return $data['Items']['Item']['DetailPageURL'];
+
+                case 'price':
+                    return $data['Items']['Item']['ItemAttributes']['ListPrice']['FormattedPrice'];
+
+                case 'rating':
+                    return $data['Items']['Item']['ItemAttributes']['AudienceRating'];
+
+                case 'genre':
+                    return $data['Items']['Item']['ItemAttributes']['Genre'];
+
+                case 'creator':
+                    $display = isset($atts['display']) ? $atts['display'] : 3;
+                    return is_array($data['Items']['Item']['ItemAttributes']['Creator']) ?
+                    implode(", ", array_slice($data['Items']['Item']['ItemAttributes']['Creator'], 0, $display)) : '';
+
+                case 'studio':
+                    return $data['Items']['Item']['ItemAttributes']['Studio'];
+
+                case 'runtime':
+                    return $data['Items']['Item']['ItemAttributes']['RunningTime'];
+
+                case 'actors':
+                    $display = isset($atts['display']) ? $atts['display'] : 3;
+                    return is_array($data['Items']['Item']['ItemAttributes']['Actor']) ?
+                    implode(", ", array_slice($data['Items']['Item']['ItemAttributes']['Actor'], 0, $display)) : '';
+            }
+        }
+    }
 }
 
 function format_date($date, $display)
 {
-	$date = new DateTime($date);
-	
-	$display = preg_split("/(yyyy|yy|mmmm|mmm|mm|m|dddd|ddd|dd|d|hh:mm|h:mm|AM\/PM|AMPM|w)/", $display, -1, PREG_SPLIT_DELIM_CAPTURE);
-	$replace = array(
-		'yyyy' => 'Y',
-		'yy' => 'y',
-		'mmmm' => 'F',
-		'mmm' => 'M',
-		'mm' => 'm',
-		'm' => 'n',
-		'dddd' => 'l',
-		'ddd' => 'D',
-		'dd' => 'd',
-		'd' => 'j',
-		'h:mm' => 'g:i',
-		'hh:mm' => 'h:i',
-		'AM/PM' => 'A',
-		'AMPM' => 'A',
-	);
+    $date = new DateTime($date);
 
-	$ret = '';
-	foreach ($display as $token) {
-		if (array_key_exists($token, $replace)) {
-			if ($replace[$token] == 'z') {
-				$date->add(new DateInterval('P1D'));
-			}
+    $display = preg_split("/(yyyy|yy|mmmm|mmm|mm|m|dddd|ddd|dd|d|hh:mm|h:mm|AM\/PM|AMPM|w)/", $display, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $replace = array(
+        'yyyy' => 'Y',
+        'yy' => 'y',
+        'mmmm' => 'F',
+        'mmm' => 'M',
+        'mm' => 'm',
+        'm' => 'n',
+        'dddd' => 'l',
+        'ddd' => 'D',
+        'dd' => 'd',
+        'd' => 'j',
+        'h:mm' => 'g:i',
+        'hh:mm' => 'h:i',
+        'AM/PM' => 'A',
+        'AMPM' => 'A',
+    );
 
-			$ret .= $date->format($replace[$token]);
+    $ret = '';
+    foreach ($display as $token) {
+        if (array_key_exists($token, $replace)) {
+            if ($replace[$token] == 'z') {
+                $date->add(new DateInterval('P1D'));
+            }
 
-			if ($token == 'w') {
-				$ret = 1 + intval($ret / 7);
-			}
-		} else {
-			$ret .= $token;
-		}
-	}
-	
-	return $ret;
+            $ret .= $date->format($replace[$token]);
+
+            if ($token == 'w') {
+                $ret = 1 + intval($ret / 7);
+            }
+        } else {
+            $ret .= $token;
+        }
+    }
+
+    return $ret;
 }
 
 function api_metadata_settings_page()
@@ -82,7 +106,7 @@ function api_metadata_settings_page()
 <div class="wrap">
      <form action="options.php" method="post">
      <h2>API Metadata Settings for Amazon</h2>
-       <?php settings_fields('api-metadata-settings'); ?>
+       <?php settings_fields('api-metadata-settings');?>
         <table class="form-table">
             <tr>
 				<td width="10%">Access Key</td>
@@ -111,52 +135,50 @@ function api_metadata_settings_page()
 
 function make_amazon_request($itemId)
 {
-	if(empty($itemId))
-	{
-		return '';
-	}
-	
-	$ret = get_transient('amazon_product_' . $itemId);
-	if($ret !== false && $ret !== '')
-	{
-		return $ret;
-	}
-	
-	$access_key_id = get_option('api-metadata-settings-amazon-accesskey');
-	$secret_key = get_option('api-metadata-settings-amazon-secretkey');
-	$tag = get_option('api-metadata-settings-amazon-tag');
-	$endpoint = get_option('api-metadata-settings-amazon-endpoint');
+    if (empty($itemId)) {
+        return '';
+    }
 
-	$uri = '/onca/xml';
+    $ret = get_transient('amazon_product_' . $itemId);
+    if ($ret !== false && $ret !== '') {
+        return $ret;
+    }
 
-	$params = array(
-		'Service' => 'AWSECommerceService',
-		'Operation' => 'ItemLookup',
-		'AWSAccessKeyId' => $access_key_id,
-		'AssociateTag' => $tag,
-		'ItemId' => $itemId,
-		'IdType' => 'ASIN',
-		'ResponseGroup' => 'ItemAttributes',
-		'Timestamp' => gmdate('Y-m-d\TH:i:s\Z')
-	);
+    $access_key_id = get_option('api-metadata-settings-amazon-accesskey');
+    $secret_key = get_option('api-metadata-settings-amazon-secretkey');
+    $tag = get_option('api-metadata-settings-amazon-tag');
+    $endpoint = get_option('api-metadata-settings-amazon-endpoint');
 
-	ksort($params);
+    $uri = '/onca/xml';
 
-	$pairs = array();
-	foreach ($params as $key => $value) {
-		array_push($pairs, rawurlencode($key).'='.rawurlencode($value));
-	}
+    $params = array(
+        'Service' => 'AWSECommerceService',
+        'Operation' => 'ItemLookup',
+        'AWSAccessKeyId' => $access_key_id,
+        'AssociateTag' => $tag,
+        'ItemId' => $itemId,
+        'IdType' => 'ASIN',
+        'ResponseGroup' => 'ItemAttributes',
+        'Timestamp' => gmdate('Y-m-d\TH:i:s\Z'),
+    );
 
-	$canonical_query_string = join('&', $pairs);
-	$signature = base64_encode(hash_hmac('sha256', "GET\n".$endpoint."\n".$uri."\n".$canonical_query_string, $secret_key, true));
-	$request_url = 'https://'.$endpoint.$uri.'?'.$canonical_query_string.'&Signature='.rawurlencode($signature);
-	
-	$xml = file_get_contents($request_url);
-	$data = json_decode(json_encode(simplexml_load_string($xml)), TRUE);
-	
-	set_transient('amazon_product_' . $itemId, $data, DAY_IN_SECONDS);
-	
-	return $data;
+    ksort($params);
+
+    $pairs = array();
+    foreach ($params as $key => $value) {
+        array_push($pairs, rawurlencode($key) . '=' . rawurlencode($value));
+    }
+
+    $canonical_query_string = join('&', $pairs);
+    $signature = base64_encode(hash_hmac('sha256', "GET\n" . $endpoint . "\n" . $uri . "\n" . $canonical_query_string, $secret_key, true));
+    $request_url = 'https://' . $endpoint . $uri . '?' . $canonical_query_string . '&Signature=' . rawurlencode($signature);
+
+    $xml = file_get_contents($request_url);
+    $data = json_decode(json_encode(simplexml_load_string($xml)), true);
+
+    set_transient('amazon_product_' . $itemId, $data, DAY_IN_SECONDS);
+
+    return $data;
 }
 
 // Init
@@ -167,8 +189,8 @@ add_action('admin_menu', function () {
 });
 
 add_action('admin_init', function () {
-	register_setting('api-metadata-settings', 'api-metadata-settings-amazon-accesskey');
-	register_setting('api-metadata-settings', 'api-metadata-settings-amazon-secretkey');
-	register_setting('api-metadata-settings', 'api-metadata-settings-amazon-tag');
-	register_setting('api-metadata-settings', 'api-metadata-settings-amazon-endpoint');
+    register_setting('api-metadata-settings', 'api-metadata-settings-amazon-accesskey');
+    register_setting('api-metadata-settings', 'api-metadata-settings-amazon-secretkey');
+    register_setting('api-metadata-settings', 'api-metadata-settings-amazon-tag');
+    register_setting('api-metadata-settings', 'api-metadata-settings-amazon-endpoint');
 });
