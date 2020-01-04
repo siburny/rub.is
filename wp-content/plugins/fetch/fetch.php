3054fe41e0df400354d6753b0086e298522f95bc
Maxim Rubis <siburny@gmail.com>
2020-01-04 00:16:57 -0500

    v0.3: cache duration; shorten to 3 sigfig

--- wp-content/plugins/fetch/fetch.php
+++ wp-content/plugins/fetch/fetch.php
@@ -2,10 +2,20 @@
 /**
  * Plugin Name: Social Media Fetch
  * Description: Fetch social media metadata
- * Version: 0.2
+ * Version: 0.3
  */
 
 const FETCH_CACHE_PREFIX = 'plugin_fetch_';
+
+if (!function_exists('_sigFig')) {
+    function _sigFig($value, $digits)
+    {
+        if ($value >= pow(10, $digits)) {
+            $answer = substr($value, 0, $digits) . preg_replace('/./', '0', substr($value, $digits));
+        }
+        return $answer;
+    }
+}
 
 function fetch_func($atts)
 {
@@ -21,7 +31,6 @@ function fetch_func($atts)
     }
 
     $cache = get_transient(FETCH_CACHE_PREFIX . $url);
-    //$cache = false;
     if ($cache !== false && !empty($cache)) {
         $data = $cache;
     } else {
@@ -40,7 +49,7 @@ function fetch_func($atts)
                     $data = substr($matches[$i], 21, -1);
                     $data = json_decode($data);
 
-                    set_transient(FETCH_CACHE_PREFIX . $url, $data, DAY_IN_SECONDS);
+                    set_transient(FETCH_CACHE_PREFIX . $url, $data, (get_option('fetch-settings-cache-duration') ? get_option('fetch-settings-cache-duration') : 7) * DAY_IN_SECONDS);
 
                     break;
                 }
@@ -62,6 +71,19 @@ function fetch_func($atts)
     switch ($atts['display']) {
         case 'followers':
             $ret = $data->entry_data->ProfilePage[0]->graphql->user->edge_followed_by->count;
+            if ($ret < 10000) {
+                $ret = number_format($ret, 0);
+            } else {
+                $ret = _sigFig($ret, 3);
+
+                if ($ret > 1000000000) {
+                    $ret = round($ret / 1000000000, 3) . 'b';
+                } else if ($ret > 1000000) {
+                    $ret = round($ret / 1000000, 3) . 'm';
+                } else if ($ret > 1000) {
+                    $ret = round($ret / 1000, 3) . 'k';
+                }
+            }
             break;
         case 'posts':
             if ($data->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges[$show]->node->shortcode) {
@@ -82,4 +104,36 @@ function fetch_func($atts)
     return '';
 }
 
+function fetch_settings_page()
+{
+    ?>
+<div class="wrap">
+    <form action="options.php" method="post">
+        <h2>Fetch Plugin Settings</h2>
+        <?php settings_fields('fetch-settings');?>
+
+        <p><?php submit_button();?></p>
+
+        <table class="form-table">
+            <tr>
+                <th style="vertial-align:top;">Cache Duration (in days):</th>
+                <td><input name="fetch-settings-cache-duration" value="<?php echo esc_attr(get_option('fetch-settings-cache-duration') ? get_option('fetch-settings-cache-duration') : 7); ?>" /></td>
+            </tr>
+        </table>
+
+    </form>
+</div>
+<?php
+
+}
+
+// Init
 add_shortcode('fetch', 'fetch_func');
+
+add_action('admin_menu', function () {
+    add_options_page('Fetch Settings', 'Fetch Settings', 'manage_options', 'fetch-settings', 'fetch_settings_page');
+});
+
+add_action('admin_init', function () {
+    register_setting('fetch-settings', 'fetch-settings-cache-duration');
+});
