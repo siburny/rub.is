2b23d328de7ec26b47fd4043001f140075c4046c
Maxim Rubis <siburny@gmail.com>
2023-12-27 00:48:47 -0500

    Add custom dark style

---
+++ wp-content/mu-plugins/custom-css.php
@@ -0,0 +1,10 @@
+<?php
+
+add_filter( 'dark_mode_dashboard_css', 'dark_mode_dashboard_css_custom');
+
+function dark_mode_dashboard_css_custom( $style ) {
+    wp_register_style( 'dark-mode-dashboard-custom', '/wp-content/themes/dark-mode-dashboard-custom.css' );
+    wp_enqueue_style( 'dark-mode-dashboard-custom' );
+    
+    return $style;
+}
