7e4fa31b1fc717158c68df85ab75d5cb247911f6
Maxim Rubis <siburny@gmail.com>
2023-12-27 11:00:14 -0500

    Add custom theme css

--- wp-content/mu-plugins/custom-css.php
+++ wp-content/mu-plugins/custom-css.php
@@ -1,6 +1,7 @@
 <?php
 
 add_filter( 'dark_mode_dashboard_css', 'dark_mode_dashboard_css_custom');
+add_filter( 'the_content', 'dark_mode_dashboard_css_custom');
 
 function dark_mode_dashboard_css_custom( $style ) {
     wp_register_style( 'dark-mode-dashboard-custom', '/wp-content/themes/dark-mode-dashboard-custom.css' );
